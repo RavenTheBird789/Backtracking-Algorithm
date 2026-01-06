dbfbb02bdd5945183f58b7ec2ef7bf40007da33a
Art3mis_XP <northwalker676@gmail.com>
2026-01-06 03:45:15 +0100

    Update backtrac.py

--- backtrac.py
+++ backtrac.py
@@ -1,28 +1,38 @@
-# Backtracking
+# Shuffling Permutation For Recursive Shuffle (Backtracking)
 import random
+import time
 
-# Initialize list of numbers and results
-nums = [1, 2, 3]
-results = []
+def shuffle():
+   # Initialize list of numbers and results
+   nums = [1, 2, 3]
+   results = []
 
-print("Start: ")
-print(f"Nums: {nums}");
-print(f"Results: {results}");
-print("");
+   print("Start: ")
+   print(f"Nums: {nums}");
+   print(f"Results: {results}");
+   print("");
 
-# Backtracking algorithm to randomly select numbers from nums and add to results
-print("Organizing numbers...");
-while nums != []:
-   ran = (random.choice(nums));
-   results.append(ran);
-   for item in results:
-      if item in nums:
-        del nums[nums.index(item)];
-        print(f"Nums: {nums}");
-        print(f"Results: {results}");
-        break;
-print("Numbers organized!");
-print("");
-print("Finish: ")
-print(f"Nums left: {nums}");
-print(f"Results: {results}");
+   # Shuffling Permutation algorithm to randomly select numbers from nums and add to results
+   print("Organizing numbers...");
+   while nums != []:
+      ran = (random.choice(nums));
+      results.append(ran);
+      for item in results:
+         if item in nums:
+           del nums[nums.index(item)];
+           print(f"Nums: {nums}");
+           print(f"Results: {results}");
+           break;
+   print("Numbers organized!");
+   print("");
+   print("Finish: ")
+   print(f"Nums left: {nums}");
+   print(f"Results: {results}");
+
+   if results != [3, 2, 1]:
+       print("Solution Is Not Reversed, Reshuffling...");
+       time.sleep(2);
+       shuffle();
+   else:
+      print("Successfully Shuffled In Reversed!");
+shuffle();
