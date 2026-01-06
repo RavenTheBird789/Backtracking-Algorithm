9c70bb1bf8f86ea307c6ccfefcaef4bde9fdb5f8
Art3mis_XP <northwalker676@gmail.com>
2026-01-06 03:47:30 +0100

    Add files via upload

---
+++ shuff_per.py
@@ -0,0 +1,28 @@
+# Shuffling Permutation
+import random
+
+# Initialize list of numbers and results
+nums = [1, 2, 3]
+results = []
+
+print("Start: ")
+print(f"Nums: {nums}");
+print(f"Results: {results}");
+print("");
+
+# Shuffling Permutation algorithm to randomly select numbers from nums and add to results
+print("Organizing numbers...");
+while nums != []:
+   ran = (random.choice(nums));
+   results.append(ran);
+   for item in results:
+      if item in nums:
+        del nums[nums.index(item)];
+        print(f"Nums: {nums}");
+        print(f"Results: {results}");
+        break;
+print("Numbers organized!");
+print("");
+print("Finish: ")
+print(f"Nums left: {nums}");
+print(f"Results: {results}");
